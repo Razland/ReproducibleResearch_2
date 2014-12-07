56514c19956b82f8252b936e756c98df836b5391
dave <anon@dontspam.me>
2014-12-07 01:24:45 -0700

    Working download, read, clean data.  Need to check regional "state" names listed in remarks/comments.

--- PA2source.R
+++ PA2source.R
@@ -14,83 +14,96 @@ downLoadData <- function(){
   }
 } 
 
-
 loadStormDat <- function(){
   downLoadData()                            ## Call to download data.
   if(!"stormDat" %in% ls()){                ## If not already loaded, read-in
     stormDat <- read.csv(targFileName)      ## csv data file directly from bzip
     ## archive.
-    chainGsub <-function(dat, pat, repl) {    ## Function to chain string replace-
-      return(gsub(pat, repl, dat))            ## ments for colnames
-    }
-    
-    insertMark <- function(hourMinStr){       ## Function to change time stamp 
-      return(paste0(strtrim(                  ## data to text, and insert ":".
-        hourMinStr,2), 
-        ":", 
-        substr(
-          hourMinStr, 3, 4)))
-    }
-    
-    colnames(stormDat) <-                     ## Rename the column headings to
-      colnames(stormDat) %>%                    ## lower case using chaining to
-      tolower() %>%                             ## modify the strings
-      chainGsub("_locati", "_location") %>%
-      chainGsub("latitude_e", "latitude_end") %>%
-      chainGsub("longitude_", "longitude_end") %>%
-      chainGsub("_azi", "_heading") %>%
-      chainGsub("bgn", "begin") %>%
-      chainGsub("evtype", "event_type") %>%
-      chainGsub("endn", "endname") %>%
-      chainGsub("state__", "state_code") %>%
-      chainGsub("stateoffic", "stateoffice") %>%
-      chainGsub("dmg", "damage") %>%
-      chainGsub("propd", "propertyd") %>%
-      chainGsub("wfo", "weatherforecastoffice")
-    colnames(stormDat)[21] <- "fujita_scale"
-    
-    stormDat$begin_date <-                      ## Change to character and remove 
-      gsub(" 0:00:00", "",                    ## superfluous time stamps
-           as.character(stormDat$begin_date))
-    stormDat$end_date <-                      ## Change to character and remove 
-      gsub(" 0:00:00", "",                    ## superfluous time stamps
-           as.character(stormDat$end_date))
-    stormDat$begin_time <-                      ## Add colon and change the time 
-      insertMark(as.character(                ## stamps to text data type.
-        stormDat$begin_time))
-    stormDat$end_time <- 
-      insertMark(as.character(
-        stormDat$end_time))
-    stormDat$time_zone <-                     ## Change time zone abbreviation to
-      toupper(                                ## text format.
-        as.character(stormDat$time_zone))
-    ## Data cleaning data -- Correction of State codes
-    stormDat[stormDat$state == "XX",          ## New York coastal events
-             7] <- "NY"
-    stormDat[stormDat$state == "SL",          ## New York St. Lawrence Seaway
-             7] <- "NY"                       ## events   
-    stormDat[stormDat$state == "PZ",          ## Indicates pacific coast. All refs
-             7] <- "CA"                       ## are to CA
-    stormDat[stormDat$state == "ST",          
-             7] <- "OH"
-    ## State code "AN" may represent "Atlantic Narrows," and should be treated 
-    ## specially in state counts: data with  
-    ## this label represent multiple states, including "DE", "NJ", "NY", "VA", 
-    ## "MD", "NC".  State code "LE" is used for Lake Erie multitstate events, 
-    ## including "OH", "NY", "MI", "PA". State code "GM" used for mulitstate 
-    ## events around the Gulf of Mexico
-    
-    stormDat[stormDat$state=="PK",            ## Pacific + Alaskan coastal areas  
-             7] <- "AK"                       ## 
-    stormDat[stormDat$state=="AK" &           ## Alaska Daylight savings time code
-               stormDat$time_zone=="ADT",       ## to modern standard code.
-             4]<-"AKDT"                       ## Correction of Alaskan Standard  
-    stormDat[stormDat$state=="AK" &           ## time codes to modern standard
-               stormDat$time_zone %in%          ## code.
-               c("AST","AKS"), 4] <- "AKST"  
-    
     return(stormDat)
   }  
 }
 
-loadStormDat()
+cleanStormDat <- function(stormDat){        ## Function makes data "tidier."
+  require(dplyr)
+  chainGsub <-function(dat, pat, repl) {    ## Function to chain string replace-
+    return(gsub(pat, repl, dat))            ## ments for colnames
+  }
+  
+  insertMark <- function(hourMinStr){       ## Function to change time stamp 
+    return(paste0(strtrim(                  ## data to text, and insert ":".
+      hourMinStr,2), 
+      ":", 
+      substr(
+        hourMinStr, 3, 4)))
+  }
+  stormDat <- stormDat[,1:36]               ## Discard record number column
+  colnames(stormDat) <-                     ## Rename the column headings to  
+    colnames(stormDat) %>%                  ## lower case using chaining to
+  tolower() %>%                             ## modify the strings to "tidier"
+  chainGsub("_locati", "location") %>%      ## form.
+  chainGsub("latitude_e", 
+            "latitudeatend") %>%  
+  chainGsub("longitude_", "longitudeatend") %>%
+  chainGsub("_azi", "heading") %>%
+  chainGsub("bgn", "begin") %>%
+  chainGsub("evtype", "eventtype") %>%
+  chainGsub("endn", "endname") %>%
+  chainGsub("state__", "statenumcode") %>%
+  chainGsub("stateoffic", "stateweatheroffice") %>%
+  chainGsub("mag", "magnitude")  %>%
+  chainGsub("dmg", "damage") %>%
+  chainGsub("propd", "propertyd") %>%
+  chainGsub("wfo", "weatherforecastoffice") %>%
+  chainGsub("_","")
+
+  colnames(stormDat)[21] <- "fujitascale"   ## Name this column without sub-
+                                            ## stitution.
+  stormDat$begin_date <-                    ## Change begin and end date types 
+    gsub(" 0:00:00", "",                    ## to plain character and remove 
+         as.character(stormDat$begin_date)) ## superfluous time stamps from
+  stormDat$end_date <-                      ## date fields. 
+    gsub(" 0:00:00", "",
+         as.character(stormDat$end_date))
+  stormDat$begin_time <-                    ## Add colon and change the event
+    insertMark(as.character(                ## beginning and ending time stamps
+      stormDat$begin_time))                 ## to text data type.
+  stormDat$end_time <- 
+    insertMark(as.character(
+      stormDat$end_time))
+
+  stormDat[stormDat$state == "XX",          ## Correction of State codes
+           7] <- "NY"                       ## New York coastal events
+  stormDat[stormDat$state == "SL",          ## New York St. Lawrence Seaway
+           7] <- "NY"                       ## events   
+  stormDat[stormDat$state == "PZ",          ## Indicates pacific coast. All refs
+           7] <- "CA"                       ## are to CA
+  stormDat[stormDat$state == "ST",          
+           7] <- "OH"
+  stormDat[stormDat$state=="PK",            ## Pacific + Alaskan coastal areas  
+           7] <- "AK" 
+  stormDat$time_zone <-                     ## Change time zone abbreviation to
+    toupper(                                ## upper case plain text format.
+      as.character(stormDat$time_zone))
+  stormDat[stormDat$state=="AK" &           ## Alaska Daylight savings time code
+             stormDat$time_zone=="ADT",     ## to modern standard code.
+           4]<-"AKDT"                       ## Correction of Alaskan Standard  
+  stormDat[stormDat$state=="AK" &           ## time codes to modern standard
+             stormDat$time_zone %in%        ## code.
+             c("AST","AKS"), 4] <- "AKST"    
+  
+  return(stormDat)
+}
+
+##stormDat <- cleanStormDat(loadStormDat())
+
+#1.  _\*Record collection began before Alaska and Hawaii statehood, and time 
+#zone standard codes have changed multiple times since the beginning of record 
+#keeping.  Several typographical errors are noted in the original data set time 
+#zone and state abbreviations._  
+
+#2.  _\*\*State code "AN" represents "Atlantic North," and should be treated 
+#specially in state counts: data with this label represent multiple states, 
+#including at least "DE", "NJ", "NY", "VA", "MD", "NC". 
+#State code "LE" is used for Lake Erie multitstate events, 
+#including "OH", "NY", "MI", "PA". State code "GM" is used for mulitstate 
+#events around the Gulf of Mexico_  
